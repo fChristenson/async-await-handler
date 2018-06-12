67839dbd059f39c49297dfc74699232efbdc7a79
Fredrik <fredrik.christenson@gmail.com>
2018-06-12 21:50:30 +0200

    init

---
+++ server.prod.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+app.get("/", async (req, res) => {
+  const foo = await Promise.resolve("1");
+  res.end(foo);
+});
+
+app.get("/fail", async (req, res) => {
+  const foo = await Promise.resolve("1");
+  throw new Error("foo error");
+  res.end(foo);
+});
+
+app.get("/safe", async (req, res, next) => {
+  try {
+    const foo = await Promise.resolve("1");
+    throw new Error("foo error");
+    res.end(foo);
+  } catch (e) {
+    next(e);
+  }
+});
+
+const catchExceptions = func => {
+  return (req, res, next) => {
+    Promise.resolve(func(req, res)).catch(next);
+  };
+};
+
+app.get(
+  "/wrap",
+  catchExceptions(async (req, res, next) => {
+    const foo = await Promise.resolve("1");
+    throw new Error("foo error");
+    res.end(foo);
+  })
+);
+
+app.use((err, req, res, next) => {
+  res.end(err.message);
+});
+
+process.on("unhandledRejection", err => {
+  // if you want a stack always throw a error
+  console.log(`Send this to error tracking: ${err.stack}`);
+  console.log("--------------------------");
+});
+
+app.listen(3000);
